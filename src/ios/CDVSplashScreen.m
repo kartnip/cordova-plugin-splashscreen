81742943fda01af905ffd85f5f109758d8d8aaeb
kartnip <kartnip@gmail.com>
2019-04-05 11:40:38 -0400

    Update CDVSplashScreen.m

--- src/ios/CDVSplashScreen.m
+++ src/ios/CDVSplashScreen.m
@@ -30,17 +30,17 @@
 
 - (BOOL)webView:(UIWebView *)theWebView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
 {
-    NSURL *url = [request URL];
-    NSString *str = url.absoluteString;
-    NSRange range = [str rangeOfString:@"http://"];
-    //HACK to make url open in safari
-    if (range.location != NSNotFound) {
-        [[UIApplication sharedApplication] openURL:url];
-        return NO;
-    }
-    else {
-        return [ super webView:theWebView shouldStartLoadWithRequest:request navigationType:navigationType ];
-    }
+	NSURL *url = [request URL];
+	
+	// Intercept the external http requests and forward to Safari.app
+	// Otherwise forward to the PhoneGap WebView
+	if ([[url scheme] isEqualToString:@"http"] || [[url scheme] isEqualToString:@"https"]) {
+		[[UIApplication sharedApplication] openURL:url];
+		return NO;
+	}
+	else {
+		return [ super webView:theWebView shouldStartLoadWithRequest:request navigationType:navigationType ];
+	}
 }
 
 - (void)pluginInitialize
