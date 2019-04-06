256e5f9b3bf93579aa603500af90e5f4cea75224
kartnip <kartnip@gmail.com>
2019-04-06 10:09:16 -0400

    Create BackButton.java

---
+++ src/android/BackButton.java
@@ -0,0 +1,12 @@
+package org.apache.cordova.splashscreen;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class BackButton extends Activity {
+       @Override
+       public void onBackPressed()
+       {
+       }
+       
+}
